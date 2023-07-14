c1b91e77275fb6670f96f4def97e9e976f88fda6
Oleg <tradecenter1771@gmail.com>
2023-07-14 13:30:41 +0300

    added task

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,5 +1,11 @@
-def are_you_playing_banjo(name):
-    if name[0] == 'r' or name[0] == 'R':
-        return f'{name} plays banjo'
-    else:
-        return f'{name} does not play banjo'
+num = 70304
+s = []
+i = len(str(num)) -1
+print(i)
+for n in str(num):
+    if n != "0":
+        s.append(n + "0" * i)
+    i -= 1
+print(s)
+g = '7' + '0' * 4
+print(g)
