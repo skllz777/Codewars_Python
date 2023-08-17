9fbc612e1b31a14ff156b7c4891b8d90d1e6f2e7
Oleg <tradecenter1771@gmail.com>
2023-08-17 19:35:10 +0300

    added task

--- 7_kyu/Find_the_middle_element.py
+++ 7_kyu/Find_the_middle_element.py
@@ -1,7 +1,2 @@
-def gimme(a):
-    if a[2] < a[0] < a[1] or a[1] < a[0] < a[2]:
-        return 0
-    if a[2] < a[1] < a[0] or a[0] < a[1] < a[2]:
-        return 1
-    if a[0] < a[2] < a[1] or a[1] < a[2] < a[0]:
-        return 2
+def gimme(input_array):
+    return input_array.index(sorted(input_array)[1])
