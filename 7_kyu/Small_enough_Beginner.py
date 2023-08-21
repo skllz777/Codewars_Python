7190e50a2671322785da1ae28c1f3e5086634800
Oleg <tradecenter1771@gmail.com>
2023-08-21 13:35:17 +0300

    added task

--- 7_kyu/Small_enough_Beginner.py
+++ 7_kyu/Small_enough_Beginner.py
@@ -1,9 +1,2 @@
 def small_enough(array, limit):
-    array1 = []
-    for number in array:
-        if number <= limit:
-            array1.append(number)
-    if len(array1) == len(array):
-        return True
-    else:
-        return False
+    return True if max(array)<=int(limit) else False
